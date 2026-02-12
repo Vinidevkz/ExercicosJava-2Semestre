1c47622e6893ccfe274ed31b0d5a62646a22e62b
Vini <viniynl710@gmail.com>
2026-02-12 17:01:07 -0300

    first commit

---
+++ src/algoritmos/Prj_120226.java
@@ -0,0 +1,15 @@
+package algoritmos;
+
+import java.util.Scanner;
+
+public class Prj_120226 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+	}
+
+}
