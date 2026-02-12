a2f85aac4eaa0c75d0335670141e87ebf921c0d9
Vini <viniynl710@gmail.com>
2026-02-12 17:56:22 -0300

    att

---
+++ src/algoritmos/ExercLt1_01.java
@@ -0,0 +1,30 @@
+/*
+ * Nome do projeto: ExercLt1_01
+ * Objetivo: Coletar o valor do lado de um quadrado, calcular sua
+ * area e apresentar o resultado.
+ * 
+ * Nome do programador: Vinicius Eduardo
+ * Data do desenvolvimento: 12/02/26
+*/
+
+package algoritmos;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class ExercLt1_01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int areaQuadrado;
+		
+		int lado = Integer.parseInt(JOptionPane.showInputDialog("Digite o lado do quadrado: "));
+		
+		areaQuadrado = lado * lado;
+		
+		JOptionPane.showMessageDialog(null, "Área do quadrado: " + areaQuadrado);
+		
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module AtividadesJava {
+	requires java.desktop;
 }
