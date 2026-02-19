f22f24ac095d13ba0867c6c370a35f587608e37e
Vinidevkz <viniynl710@gmail.com>
2026-02-19 17:39:20 -0300

    add

---
+++ src/algoritmos/parte2 decisao/EstDeq_Lt01_18.java
@@ -0,0 +1,32 @@
+/*
+ * Nome do projeto: EstDec_Lt01_18
+ * Objetivo: Coleta dois numeros e mostra  diferença do maior
+ * pelo menor. 
+ * 
+ * Nome do programador: Vinicius Eduardo
+ * Data do desenvolvimento: 19/02/26
+*/
+package exercicosparte2;
+import javax.swing.*;
+	
+public class EstDeq_Lt01_18 {
+
+	public static void main(String[] args) {
+		int n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+		int n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo numero: "));
+		
+		int n3;
+		
+		if(n1 > n2)
+			{
+			n3 = n1 - n2;
+			}
+		else
+			{
+				n3 = n2 - n1;
+			}
+		
+		JOptionPane.showMessageDialog(null, "Diferença:" + n3);
+	}
+
+}
---
+++ src/algoritmos/parte2 decisao/EstDeq_Lt01_21.java
@@ -0,0 +1,24 @@
+/*
+ * Nome do projeto: EstDec_Lt01_20
+ * Objetivo: Receber 3 coeficientes de uma equação de segundo
+ * grau e verificar a existencia de raizes.
+ * 
+ * Nome do programador: Vinicius Eduardo
+ * Data do desenvolvimento: 19/02/26
+*/
+package exercicosparte2;
+import java.util.Arrays;
+
+import javax.swing.*;
+import static java.lang.Math.pow;
+
+public class EstDeq_Lt01_21 {
+	public static void main(String[] args) {
+		double n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: "));
+		double n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: "));
+		double n3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: "));
+		
+		
+	
+	}
+}
